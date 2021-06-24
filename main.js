6afd48d8ba19b6105c4d8119648eadfdaea63d35
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-24 12:22:26 +0530

    First code

--- main.js
+++ main.js
@@ -6,31 +6,33 @@ global.task = ' ';
 global.body = ' ';
 var main = function(req, res)
 {
-    fs.readFile('login.html', function(err, data){
-        
-        var q = url.parse(req.url, true).query;
-        body = task + data;
-        if(req.url === '/')
-        {
+    if(req.url != '/favicon.ico')
+    {
+        fs.readFile('login.html', function(err, data){
             
-        }
-        else if(q.clear === 'clear')
-        {
-            task = ' ';
-            body = ' ';
-            body += data;
-        }
-        else if(q.task)
-        { 
-            var date = Date();
-            
-            task += `<p class="font-face: sans-serif;">`+ q.task +`<br><a style="color: gray;">`+ String(date) +`</a></p><br><hr>`;
+            var q = url.parse(req.url, true).query;
             body = task + data;
-        }       
-        res.write(body);
-        res.end();
-    });
-    
+            if(req.url === '/')
+            {
+                
+            }
+            else if(q.clear === 'clear')
+            {
+                task = ' ';
+                body = ' ';
+                body += data;
+            }
+            else if(q.task)
+            { 
+                var date = Date();
+                
+                task += `<p class="font-face: sans-serif;">`+ q.task +`<br><a style="color: gray;">`+ String(date) +`</a></p><br><hr>`;
+                body = task + data;
+            }       
+            res.write(body);
+            res.end();
+        });
+    }
 }
 
 http.createServer(main).listen(process.env.PORT || 4200);
