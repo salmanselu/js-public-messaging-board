257e2f65a5c3fe5a6ca337fea2c5da9ec67d7447
Salman Nazeer <salmanselu2002@gmail.com>
2021-06-24 15:37:28 +0530

    corrected message doubling bug

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-var http = require("http");
+var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
 global.task = ' ';
 global.body = ' ';
 var main = function(req, res)
-{
-    if(req.url != '/favicon.ico')
-    {
-        fs.readFile('login.html', function(err, data){
+{     
+    fs.readFile('login.html', function(err, data){
+        
+        var q = url.parse(req.url, true).query;
+        body = task + data;
+        if(req.url === '/')
+        {
             
-            var q = url.parse(req.url, true).query;
+        }
+        else if(q.clear === 'clear')
+        {
+            task = ' ';
+            body = ' ';
+            body += data;
+        }
+        else if(q.task)
+        { 
+            var date = Date();
+            
+            task += `<p class="font-face: sans-serif;">`+ q.task +`<br><a style="color: gray;">`+ String(date) +`</a></p><br><hr>`;
             body = task + data;
-            if(req.url === '/')
-            {
-                
-            }
-            else if(q.clear === 'clear')
-            {
-                task = ' ';
-                body = ' ';
-                body += data;
-            }
-            else if(q.task)
-            { 
-                var date = Date();
-                
-                task += `<p class="font-face: sans-serif;">`+ q.task +`<br><a style="color: gray;">`+ String(date) +`</a></p><br><hr>`;
-                body = task + data;
-            }       
-            res.write(body);
-            res.end();
-        });
-    }
+            /////////////////
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            ////////////////
+        }       
+        res.write(body);
+        const headers = res.getHeaders();
+        console.log(headers);
+        res.end();    
+    });
 }
-
 http.createServer(main).listen(process.env.PORT || 4200);
