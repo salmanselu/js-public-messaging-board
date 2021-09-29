d3326dce7dac2cab9d2a044bf7976c937b8d6da9
=Salman nazeer <salmanselu2002@gmail.com>
2021-09-29 22:27:33 +0530

    Reworked entire thing

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
 var http = require('http');
-var fs = require('fs');
+
 var url = require('url');
 
-global.task = ' ';
-global.body = ' ';
+messageData = {message:[]};
 var main = function(req, res)
-{     
-    fs.readFile('login.html', function(err, data){
-        
-        var q = url.parse(req.url, true).query;
-        body = task + data;
-        if(req.url === '/')
-        {
-            
-        }
-        else if(q.clear === 'clear')
-        {
-            task = ' ';
-            body = ' ';
-            body += data;
-        }
-        else if(q.task)
-        { 
-            var date = Date();
-            
-            task += `<p class="font-face: sans-serif;">`+ q.task +`<br><a style="color: gray;">`+ String(date) +`</a></p><br><hr>`;
-            body = task + data;
-            /////////////////
-            res.statusCode=302;
-            res.setHeader('Location','/');
-            ////////////////
-        }       
-        res.write(body);
-        const headers = res.getHeaders();
-        console.log(headers);
-        res.end();    
-    });
+{   
+   var q = url.parse(req.url, true).query;
+   if(q.pathname !== '/'){  
+      if(q.message !== undefined && q.message !== ""){
+         messageData.message.push(q.message);
+         console.log(messageData);
+      }
+      console.log(q);
+      res.write(JSON.stringify(messageData));
+      res.end();
+   }
 }
 http.createServer(main).listen(process.env.PORT || 4200);
